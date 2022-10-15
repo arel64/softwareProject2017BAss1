5e3df440d5648ec07b52299f22496bba0f2b711e
Arel Sharon <arel.sharon@gmail.com>
2022-10-15 23:42:20 +0300

    finshed logic

--- main.c
+++ main.c
@@ -15,6 +15,7 @@ int main(){
     scanf_s("%x",&input);
     
     unsigned tempdigit;
+    
     unsigned digitCount=0;
 
     while(input > 0){
@@ -22,40 +23,19 @@ int main(){
         converted += tempdigit * pow(base,digitCount++);
         input /= 16;
     }
-    /*
-    digitCount = 0;
-    while (converted > 0) {
-       tempdigit = converted % (int)(pow(targetNum,digitCount));
-       targetNum += tempdigit * pow(16,digitCount);
-       converted -= pow(targetNum,digitCount);
-
-       digitCount++;
-    }
-    */
     
-    while(converted > 0){
-        tempdigit = converted%10;
-        printf("%d aaa %d",converted,converted%10);
-
-        targetNum += tempdigit * pow(base,digitCount++);
-        converted /= 10;
-        printf("%d \n",tempdigit);
-
-    }
-    
-    /*
-    int decLen = log10(targetNum) + 1;
+    int decLen = log10(converted) / log10(targetBase) + 1;
     int digitArr[decLen];
 
-    for(int i=decLen;i>0;i--){
 
+    for(int i=decLen;i>=0;i--){
         digitArr[i] = converted / pow(targetBase,i);
-        converted -= converted*digitArr[i];
+        converted -= pow(targetBase,i)*digitArr[i];
     }
-    printf("\n");
+
     for(int i = decLen; i>=0 ;i-- ){
-        printf("%x",digitArr[decLen]);
+        printf("%x",digitArr[i]);
     }
-    */
+    
     
 }
